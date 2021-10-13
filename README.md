ee94bb5343d04501ecaa29b5c1e5219b6ec3255b
attoumbre <juvenalak@gmail.com>
2021-10-13 13:28:00 +0200

    Reorganisation du projet et ajout de la classe SelectionImpl

---
+++ README.md
@@ -0,0 +1,3 @@
+# TpAco
+
+Dans le cadre de notre tp d'ACO, il nous est demandé de programmer un éditeur de texte. 
---
+++ src/fr/istic/aco/editor/SelectionImpl.java
@@ -0,0 +1,41 @@
+package fr.istic.aco.editor;
+
+public class SelectionImpl implements Selection {
+
+	@Override
+	public int getBeginIndex() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public int getEndIndex() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public int getBufferBeginIndex() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public int getBufferEndIndex() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public void setBeginIndex(int beginIndex) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void setEndIndex(int endIndex) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
